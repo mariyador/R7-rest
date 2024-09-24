3774a7ff20366f54035f8184f90817ee7c2be6e2
mariyador <aniknorod@gmail.com>
2024-09-23 22:06:59 -0400

    More REST APIs

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
                  sessions: 'users/sessions',
                  registrations: 'users/registrations'
              }
-             
+
+  namespace :api do
+    namespace :v1 do
+      resources :members do
+        resources :facts
+      end
+    end
+  end
+
   get '/test', to: 'test#show'
 end
